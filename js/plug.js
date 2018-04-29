c9aab2215d6660c2a28d1e2a17ce12d67e1eaeaa
abdelrahman adel <38409101+abdelrahmanadel30@users.noreply.github.com>
2018-04-29 03:36:15 +0200

    MYlab

---
+++ js/plug.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+$("nav .logo").mouseenter(function(){
+$(this).css("color"," #1da1f2")
+
+});
+ $("nav .logo").mouseleave(function(){
+$(this).css("color"," #DDD")
+
+});   
+   
+$("header p ").mouseenter(function(){
+$(this).css("color","#1da1f2")
+
+});    
+   $("header p ").mouseleave(function(){
+$(this).css("color","white")
+}); 
+
+
+});
